095041d416dde6f87877392d0df154d1cc2aba87
long2ge <444533452@qq.com>
2020-05-10 15:49:56 +0800

    add 增加后台用户登陆和退出路由

---
+++ Modules/Admin/Database/Migrations/2019_12_08_141500_create_users.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $dbConnections = config('database.connections.admin.connection');
+        $tableNames = 'users';
+
+        Schema::connection($dbConnections)
+            ->create($tableNames, function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('username')->unique()->nullable()->comment('账号名');
+                $table->string('name')->comment('用户昵称');
+                $table->string('email')->unique()->nullable();
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('password')->comment('密码');
+                $table->string('avatar')->nullable()->comment('头像');
+                $table->string('real_name')->nullable()->comment('真名');
+                $table->boolean('sex')->nullable()->comment('性别');
+                $table->date('birthday')->nullable()->comment('生日');
+                $table->string('phone', 20)->unique()->nullable()->comment('手机号码');
+                $table->integer('role_id')->unsigned()->default(0)->comment('角色id');
+                $table->tinyInteger('status')->default(1)->comment('状态 0 冻结, 1正常');
+                $table->softDeletes();
+                $table->timestamps();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $dbConnections = config('database.connections.admin.connection');
+        $tableNames = 'users';
+        Schema::connection($dbConnections)->dropIfExists($tableNames);
+    }
+}
---
+++ Modules/Admin/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: long
+ * Date: 2019/9/7
+ * Time: 1:03 AM
+ */
+
+namespace Modules\Admin\Http\Controllers\Api;
+
+use App\Services\AuthorizationManageServer;
+use Illuminate\Http\Request;
+use Modules\Admin\Http\Controllers\AdminAppController;
+use Modules\Admin\Repositories\UserRepository;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * 登录控制器
+ * Class LoginController
+ * @package App\Http\Controllers\User\Auth
+ */
+class AuthController extends AdminAppController
+{
+    /**
+     * 登陆
+     * User: long
+     * Date: 2020/5/10 3:16 PM
+     * Describe:
+     * @param ServerRequestInterface $request
+     * @return mixed
+     * @throws \Illuminate\Validation\ValidationException
+     * @throws \Laravel\Passport\Exceptions\OAuthServerException
+     */
+    public function login(ServerRequestInterface $request)
+    {
+        $attributes = (array) $request->getParsedBody();
+        $attributes = [
+            'username' => $attributes['username'] ?? null,
+            'password' => $attributes['password'] ?? null,
+        ];
+
+        $this->validateWithArray($attributes, [
+            'username' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $AuthorizationManageServer = new AuthorizationManageServer(new UserRepository());
+
+        return $AuthorizationManageServer->login($request, $attributes['username'], $attributes['password']);
+    }
+
+    /**
+     * 退出登陆
+     * User: long
+     * Date: 2020/5/10 3:17 PM
+     * Describe:
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+
+        $request->user()->logout();
+
+        return response()->noContent();
+    }
+
+}
