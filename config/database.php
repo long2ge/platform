2a4136f5f30454994b762f9fb5e229da628e131b
long2ge <444533452@qq.com>
2020-06-08 16:49:51 +0800

    mod 1 - 3

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'core'),
 
     /*
     |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ return [
 
     'connections' => [
 
-        'mysql' => [
+        'core' => [
             'driver'      => 'mysql',
             'host'        => env('DB_HOST', 'localhost'),
             'port'        => env('DB_PORT', 3306),
@@ -48,6 +48,21 @@ return [
             'timezone'    => env('DB_TIMEZONE', '+00:00'),
             'strict'      => env('DB_STRICT_MODE', false),
             'connection'  => env('DB_CONNECTION', 'core'),
+        ],
+
+        'admin' => [
+            'driver'      => 'mysql',
+            'host'        => env('DB_HOST', 'localhost'),
+            'port'        => env('DB_PORT', 3306),
+            'database'    => env('DB_ADMIN_DATABASE', 'forge'),
+            'username'    => env('DB_USERNAME', 'forge'),
+            'password'    => env('DB_PASSWORD', ''),
+            'charset'     => env('DB_CHARSET', 'utf8'),
+            'collation'   => env('DB_COLLATION', 'utf8_unicode_ci'),
+            'prefix'      => env('DB_PREFIX', ''),
+            'timezone'    => env('DB_TIMEZONE', '+00:00'),
+            'strict'      => env('DB_STRICT_MODE', false),
+            'connection'  => env('DB_ADMIN_CONNECTION', 'admin'),
         ],
 
     ],
