3876ffdb3fb53cdb2186fdbf2e57b1a0952eb9ae
long2ge <444533452@qq.com>
2020-06-09 16:51:29 +0800

    mod 修改redis连接测试工具

--- public/utils/redis_connect_test.php
+++ public/utils/redis_connect_test.php
@@ -6,29 +6,6 @@
  * Time: 3:46 PM
  */
 
-
-//require '../../bootstrap/app.php';
-
-try {
-// dd(1);
-    $redis = new Redis();
-//连接
-    $redis->connect('redis-php:6379', 6379);
-    var_dump('redis-php:6379 ping : ' . $redis->ping());
-} catch (\Exception $e) {
-    var_dump($e);
-}
-
-//$redis->auth("gdjztw-86yqy"); //密码验证
-/*
-$redis->set("Redis_hrsas_wx_access_token",null); die();
-$ticket = $redis->get("Redis_hrsas_wx_access_token");
-var_dump($ticket);die();
-*/
-
-
-var_dump(111111111111111);
-
 //检测是否连接成功
 
 try {
